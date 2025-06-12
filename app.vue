ada4573500aed3a418f9d342a0544753213ea286
kizuyoko <kizuyoko@gmail.com>
2025-06-12 15:08:43 +0200

    Favicon, Font Family

--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="min-h-screen flex items-center justify-center bg-gray-100">
-    <h1 class="text-4xl font-bold text-blue-600">A Project with Vue, TypeScript, Nuxt, and Tailwind has started!!!</h1>
+  <div class="flex flex-row items-center justify-center min-h-screen bg-gray-100">
+    <h2 class="text-2xl">This is a heading</h2>
+    <button class="px-4 py-2 text-white bg-blue-600 rounded">Click me</button>
+    <nav class="text-gray-700">Nav Item</nav>
+    <footer class="text-sm text-gray-500">Footer content</footer>
   </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        grotesk: ['"Space Grotesk"', 'sans-serif'],
+      },
+      fontWeight: {
+        semibold: 600,
+        bold: 700,
+      }
+    },
+  },
+  darkMode: 'class', 
   content: [
     './app.vue',
     './components/**/*.{vue,js,ts}',
