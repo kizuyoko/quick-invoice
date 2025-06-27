18d6af3288e275064b839300a65409ae9f1e5a4d
kizuyoko <kizuyoko@gmail.com>
2025-06-27 15:24:19 +0200

    favicon

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">  
 import Header from './components/Header.vue';
 import Footer from './components/Footer.vue';
+import { useHead } from '@vueuse/head';
+useHead({
+  link: [
+    { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+  ]
+})
 </script>
 
 <template>
